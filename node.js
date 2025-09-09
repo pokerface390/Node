0e05a9ce022920328e72b94688cc8e6ad39a5da1
JuanCarlosHdzVideos <metin2chiri@gmail.com>
2025-09-09 17:05:38 -0600

    Actualización de apache.html con nuevo diseño

---
+++ node.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer((req, res) => {
+    if(req.url=='/node.html'){
+        const filepath = path.join(__dirname, '../ubuntuServer/node.html');
+        fs.readFile(filepath, (err, data)=>{
+            if(err){
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end('<h1>404 Not Found</h1>');
+            }
+            else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+            }
+        });
+    }
+    else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hola mi profe bernardo desde node!');
+    }
+}).listen(5000,()=>{
+    console.log('Servidor Node.js corriendo en http://localhost:5000');
+});
